f87c76ee4737f9d11ccb891a170ac20538213dca
tabing16 <tabing16@users.noreply.github.com>
2023-06-07 16:28:07 +0800

    Added class Dog

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,17 @@
-package src.main.java;
-
 import src.main.java.Customer;
 
 import java.util.Scanner;
 //add comment
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer("Tim",1000.00, "tim@email.com");
-        System.out.println(customer.getName());
-        System.out.println(customer.getCreditLimit());
-        System.out.println(customer.getEmail());
+//        Customer customer = new Customer("Tim",1000.00, "tim@email.com");
+//        System.out.println(customer.getName());
+//        System.out.println(customer.getCreditLimit());
+//        System.out.println(customer.getEmail());
+        Dog dog1 = new Dog("Rex");
+        Dog dog2 = new Dog("Fluffy");
+
+        dog1.printName();
+        dog2.printName();
     }
 }
