2ed1f8290eed21ebf5e768aab1a7cce1b079d96d
tabing16 <tabing16@users.noreply.github.com>
2023-06-14 15:41:51 +0800

    Added Worker Class

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -5,17 +5,35 @@ import java.time.Year;
 public class Worker {
     private String name;
     private String birthDate;
-    private String endDate;
+    protected String endDate;
+
+    public Worker() {
+    }
+
+    public Worker(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
 
     public int getAge(){
         return Year.now().getValue() - Integer.parseInt(this.birthDate);
     }
 
     public double collectPay(){
-        return 2000.00;
+        return 0.0;
     }
 
     public void terminate(String endDate){
+        this.endDate = endDate;
         System.out.println("Termination date: " + endDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
     }
 }
